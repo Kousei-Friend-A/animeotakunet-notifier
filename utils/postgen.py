3d447c9e676cea69ed7172ab70341fc01c5c7949
Friend A <101492420+Kousei-Friend-A@users.noreply.github.com>
2024-06-03 11:56:25 +0530

    Update postgen.py

--- utils/postgen.py
+++ utils/postgen.py
@@ -3,7 +3,7 @@ from utils.screenshot import get_screenshot
 
 def get_post(ep, title):
     id, ep_num = ep.split("-episode-")
-    url = f"https://animedex.pages.dev/episode?anime={id}&episode={ep_num}"
+    url = f"https://animeotakunet.vercel.app/episode?anime={id}&episode={ep_num}"
     img = get_screenshot(id, ep_num)
 
     text = f"""
