d8c38e1339dd1c1a2bf07b1dfd2f6a091b7b1c66
Friend A <101492420+Kousei-Friend-A@users.noreply.github.com>
2024-06-03 13:13:48 +0530

    Update postgen.py

--- utils/postgen.py
+++ utils/postgen.py
@@ -1,10 +1,10 @@
-from utils.screenshot import get_screenshot
+from utils.screenshot import screenshot
 
 
 def get_post(ep, title):
     id, ep_num = ep.split("-episode-")
     url = f"https://animeotakunet.vercel.app/episode?anime={id}&episode={ep_num}"
-    img = get_screenshot(id, ep_num)
+    img = screenshot(id, ep_num)
 
     text = f"""
 📌 **New Release**
